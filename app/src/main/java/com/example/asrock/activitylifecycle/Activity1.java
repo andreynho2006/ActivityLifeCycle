d7840fe52399bbc1a14a5ba3aa9147a6e2346b9a
andreynho2006 <andreynho2006@gmail.com>
2018-12-06 16:35:13 +0200

    add tags for diferent states of the app

--- app/src/main/java/com/example/asrock/activitylifecycle/Activity1.java
+++ app/src/main/java/com/example/asrock/activitylifecycle/Activity1.java
@@ -14,4 +14,40 @@ public class Activity1 extends Activity {
         setContentView(R.layout.activity_1);
         Log.d(TAG1, "STATUS: onCreate");
     }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(TAG1, "STATUS: onRestart()");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG1, "STATUS: onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG1, "STATUS: onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG1, "STATUS: onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG1, "STATUS: onStop()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG1, "STATUS: onDestroy()");
+    }
 }
